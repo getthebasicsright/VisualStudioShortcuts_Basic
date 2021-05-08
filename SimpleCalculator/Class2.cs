70ad0aa69e744edc4010103aa6b15bc263bc4759
Harshit Agarwal <harshitdynamite@gmail.com>
2021-05-09 00:43:42 +0530

    Initial Commit

---
+++ SimpleCalculator/Class2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator1
+{
+    class Class2
+    {
+        public Class2()
+        {
+
+        }
+
+        public int MyProperty { get; set; }
+    }
+}
---
+++ SimpleCalculator/Program1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace SimpleCalculator1
+{
+    class Program1
+    {
+        // indentaiton = Ctrl K+d
+        // multiline writing = Shift + Alt + Up/Down arrow key
+        // Populate Properties + AutoComplete = Ctrl + Shift
+        //Constructor = ctor -> Tab -> tab
+        //Poperty = prop -> Tab -> tab
+        //Renaming Vars = Ctrl + H
+        //Renaming a Class = F2 on the file name.
+        //Build -> Ctrl + Shift + B
+        //Run + Debug = F5S
+        //Save multiple file = Ctrl +Shift + S
+
+        public Program1()
+        {
+
+        }
+        public string NameOfTHeProp { get; set; }
+
+        static void Main(string[] args)
+        {
+            int number1 = 2;
+            int num2 = 2;
+            var result = Sum(number1, num2);
+            string v = "something else";
+            
+        }
+
+        public static int Sum(int number1, int num2)
+        {
+            return number1 + num2;
+        }
+
+        public static int Sub(int number1, int num2)
+        {
+            return number1 - num2;
+        }
+        public static int Multiple(int number1, int num2)
+        {
+            return number1 * num2;
+        }
+
+        public static int DivideBy(int number1, int num2)
+        {
+            return number1 / num2;
+        }
+
+    }
+}
